2b899d8a47741970783cff43341445bdd13bf509
kinari321 <abcdream321@yahoo.co.jp>
2022-12-03 06:23:06 +0900

    chore: spring initializr でプロジェクト作成

---
+++ src/main/kotlin/dev/sunabak0/akiyadego/AkiyadegoApplication.kt
@@ -0,0 +1,11 @@
+package dev.sunabak0.akiyadego
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class AkiyadegoApplication
+
+fun main(args: Array<String>) {
+	runApplication<AkiyadegoApplication>(*args)
+}
---
+++ src/test/kotlin/dev/sunabak0/akiyadego/AkiyadegoApplicationTests.kt
@@ -0,0 +1,13 @@
+package dev.sunabak0.akiyadego
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class AkiyadegoApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
