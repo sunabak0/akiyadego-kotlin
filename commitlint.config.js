823adf59901cc273f37f5d4c844551c162eedcb2
Shohei SUNAGAWA <shohei.sunagawa@crowdworks.co.jp>
2022-12-03 17:10:11 +0900

    style: prettier をかける

--- commitlint.config.js
+++ commitlint.config.js
@@ -12,12 +12,12 @@ module.exports = {
      * リポジトリ
      * - https://github.com/conventional-changelog/commitlint/tree/master/%40commitlint/config-conventional
      */
-        '@commitlint/config-conventional'
+    "@commitlint/config-conventional",
   ],
-    /**
-     * helpUrl
-     *
-     * lintに引っかかった時に見せるURL
-     */
+  /**
+   * helpUrl
+   *
+   * lintに引っかかった時に見せるURL
+   */
   helpUrl: "https://www.conventionalcommits.org/ja/v1.0.0/",
 };
