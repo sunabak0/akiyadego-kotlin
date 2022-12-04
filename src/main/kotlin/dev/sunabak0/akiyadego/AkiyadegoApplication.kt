6f4a66495355c8c9767f18cdcda0789594746d89
kinari321 <abcdream321@yahoo.co.jp>
2022-12-04 15:31:21 +0900

    ci: 説明のない public クラス・関数・プロパティを禁止するルールを追加

--- src/main/kotlin/dev/sunabak0/akiyadego/AkiyadegoApplication.kt
+++ src/main/kotlin/dev/sunabak0/akiyadego/AkiyadegoApplication.kt
@@ -3,9 +3,19 @@ package dev.sunabak0.akiyadego
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
+/**
+ * Akiyadego application
+ *
+ * @constructor Create empty Akiyadego application
+ */
 @SpringBootApplication
 class AkiyadegoApplication
 
+/**
+ * Main
+ *
+ * @param args
+ */
 fun main(args: Array<String>) {
     @Suppress("SpreadOperator")
     runApplication<AkiyadegoApplication>(*args)
