ce5de20e5905e3f1d3a2c7769b68f8c0f58180b1
kinari321 <abcdream321@yahoo.co.jp>
2023-01-14 06:49:33 +0900

    feat: get top の実装

--- src/test/kotlin/dev/sunabak0/akiyadego/api/integration/ApplicationTest.kt
+++ src/test/kotlin/dev/sunabak0/akiyadego/api/integration/ApplicationTest.kt
@@ -4,6 +4,8 @@ import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.TestInstance
+import org.skyscreamer.jsonassert.JSONAssert
+import org.skyscreamer.jsonassert.JSONCompareMode
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
 import org.springframework.boot.test.context.SpringBootTest
@@ -34,10 +36,58 @@ class ApplicationTest(@Autowired val mockMvc: MockMvc) {
 
         /**
          * then:
+         * - ステータスコードを比較
+         * - レスポンスボディを比較
          */
         val expectedStatus = HttpStatus.OK.value()
         val expectedResponseBody = "{}"
         assertThat(actualStatus).isEqualTo(expectedStatus)
         assertThat(actualResponseBody).isEqualTo(expectedResponseBody)
+    }
+
+    @Test
+    @DisplayName("GET /top のテスト")
+    fun allPostTest() {
+        /**
+         * given:
+         */
+
+        /**
+         * when:
+         */
+        val response = mockMvc.get("/top") {
+            contentType = MediaType.APPLICATION_JSON
+        }.andReturn().response
+        val actualStatus = response.status
+        val actualResponseBody = response.contentAsString
+
+        /**
+         * then:
+         * - ステータスコードを比較
+         * - レスポンスボディを比較
+         */
+        val expectedStatus = HttpStatus.OK.value()
+        val expectedResponseBody = """
+            {
+                "posts":[
+                    {
+                        "id":1,
+                        "imagePath":"",
+                        "title":"",
+                        "category":"",
+                        "prefecture":"",
+                        "description":"",
+                        "userId":"",
+                        "createdAt":""
+                    }
+                ],
+            }
+        """.trimIndent()
+        assertThat(actualStatus).isEqualTo(expectedStatus)
+        JSONAssert.assertEquals(
+            expectedResponseBody,
+            actualResponseBody,
+            JSONCompareMode.NON_EXTENSIBLE
+        )
     }
 }
