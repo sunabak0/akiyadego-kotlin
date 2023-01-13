9df6c5ebbe7bf6fef62b40c73832ba266d1af0e9
Kinari <abcdream321@yahoo.co.jp>
2023-01-14 07:37:56 +0900

    feat: get top の実装 (#114)

--- src/main/kotlin/dev/sunabak0/akiyadego/presentation/ApplicationController.kt
+++ src/main/kotlin/dev/sunabak0/akiyadego/presentation/ApplicationController.kt
@@ -1,6 +1,8 @@
 package dev.sunabak0.akiyadego.presentation
 
 import dev.sunabak0.akiyadego.openapi.generated.controller.DefaultApi
+import dev.sunabak0.akiyadego.openapi.generated.model.Post
+import dev.sunabak0.akiyadego.openapi.generated.model.PostResponse
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.RestController
@@ -13,5 +15,25 @@ import org.springframework.web.bind.annotation.RestController
 class ApplicationController : DefaultApi {
     override fun rootGet(): ResponseEntity<Unit> {
         return ResponseEntity(Unit, HttpStatus.OK)
+    }
+
+    override fun allPost(): ResponseEntity<PostResponse> {
+        return ResponseEntity(
+            PostResponse(
+                posts = listOf(
+                    Post(
+                        id = 1,
+                        imagePath = "",
+                        title = "",
+                        category = "",
+                        prefecture = "",
+                        description = "",
+                        userId = "",
+                        createdAt = ""
+                    ),
+                )
+            ),
+            HttpStatus.OK
+        )
     }
 }
